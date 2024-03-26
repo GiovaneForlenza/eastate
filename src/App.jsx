08b317a2cb89365af9624a9efa676d572ff6386e
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2024-03-26 18:01:22 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div className="bg-[#FAFAF8]">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return <div className="border w-screen">
+   <div className="left">
+    <div className="logo"></div>
+    <div className="title">Eastate</div>
+   </div>
+   <div className="right"></div>
+  </div>;
+
+}
+
+export default Header;
